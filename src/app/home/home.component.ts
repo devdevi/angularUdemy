bf7b45c0779af07e3b0eb9da6bb885f7833726b7
visidevi <32283668+visidevi@users.noreply.github.com>
2018-04-17 01:02:32 -0300

    APII DATA ON HTML

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  posts1: object;
   constructor(private http: HttpClient) {
-  // here we call de Api call
-
+    // Eso de ariba lo hacemos para poder hacer una llamada de http
+    // here we call de Api call
+    //  Usamos el constructor para inyectar data o algo asi
   }
-
   ngOnInit() {
+    // ESTO ES UN CICLO DE VIDA
+    this.http.get(`https://jsonplaceholder.typicode.com/posts`).subscribe(posts1 => {
+      this.posts1 = posts1;
+      console.log(this.posts1)
+    })
+
   }
 
 }
